1d239535c0ecf06cf0e1d6d9a4e1ab8b21947880
Brian Capouch <brianc@saintjoe.edu>
2014-11-28 09:16:44 +0000

    Directory to hold admin scripts

---
+++ scripts/create_sequence.js
@@ -0,0 +1,34 @@
+db.counters.insert(
+   {
+      _id: "cemetery",
+      seq: 0,
+   }
+)
+
+db.counters.insert(
+   {                                                                            
+      _id: "geolocation",
+      seq: 0,                                                                   
+   }
+)
+
+db.counters.insert(
+   {                                                                            
+      _id: "marker",
+      seq: 0,                                                                   
+   }
+)
+
+db.counters.insert(
+   {                                                                            
+      _id: "person",
+      seq: 0,                                                                   
+   }
+)
+
+db.counters.insert(
+   {                                                                            
+      _id: "reading",
+      seq: 0,                                                                   
+   }
+)
