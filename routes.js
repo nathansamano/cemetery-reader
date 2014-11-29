8b7f0c00c131365872ed59f9709504448f26e0fb
Brian Capouch <brianc@sjccs142.saintjoe.edu>
2014-11-28 20:12:48 -0500

    Send back ID of newly created object

--- routes.js
+++ routes.js
@@ -43,8 +43,7 @@ configRoutes = function ( app, server ) {
 
   app.post( '/:obj_type/create', function ( request, response ) {
 
-    // Do you smell a kludge here?
-    // Get next ID for appropriate collection
+    // Get next ID for appropriate collection from sequence
     var dbName = request.params.obj_type;
     dbHandle.collection(
         'counters',
@@ -61,6 +60,8 @@ configRoutes = function ( app, server ) {
             {new: true, fsync: true},
         function(err, doc){
           console.log('Retval #1: ' + doc.seq);
+	  
+	  // Embed insertion query because of async op
 	  dbHandle.collection(
              request.params.obj_type,
              function ( outer_error, collection ) {
@@ -68,36 +69,36 @@ configRoutes = function ( app, server ) {
                  options_map = { safe: true },
                  obj_map     = request.body;
        
-       	  // Insert false values for unsent booleans
-       	  // Is there a better way of doing this??  Tell me please!!
-       	  console.log(request.params.obj_type);
-       	  var inputMap = JSON.parse(obj_map.json);
-       	  console.log('Input map: ' + inputMap);
-       	  check_map = booleans[request.params.obj_type];
-       	  for ( key_name in check_map ) {
-       		if ( !(inputMap.hasOwnProperty( key_name )) ) {
-       		  inputMap[key_name] = check_map[key_name];
-       		  }
-       	  	};
-       	  // Flush contents of check_map for next run
-       	  for (var member in check_map) delete check_map[member];
+       	  	 // Insert false values for unsent booleans
+       	  	 // Is there a better way of doing this??  Tell me please!!
+       	  	 console.log(request.params.obj_type);
+       	  	 var inputMap = JSON.parse(obj_map.json);
+       	  	 console.log('Input map: ' + inputMap);
+       	 	 check_map = booleans[request.params.obj_type];
+       	  	 for ( key_name in check_map ) {
+       		    if ( !(inputMap.hasOwnProperty( key_name )) ) {
+       		       inputMap[key_name] = check_map[key_name];
+       		       }
+       	  	    };
+       	  	 // Flush contents of check_map for next run
+       	  	 for (var member in check_map) delete check_map[member];
        
-       	  // Set primary key (depending on object type)
-       	  inputMap['_id'] = doc.seq;
-       	  console.log(inputMap);
+       	  	 // Set primary key (depending on object type)
+       	  	 inputMap['_id'] = doc.seq;
+       	  	 console.log(inputMap);
        
-       	  // Write full record to database, including generated key
+       	  	 // Write full record to database, including generated key
                  collection.insert(
                    inputMap,
                    options_map,
                    function ( inner_error, result_map ) {
-       	      // I think we should return the _id here, if possible
-       	      // But ATM I don't know how. . . 
-                     response.send( inputMap );
+       	             // I think we should return the _id here, if possible
+       	             // But ATM I don't know how. . . 
+                     response.json(200, 'Inserted new item with id of ' + doc.seq );
                      }
-                 ); // end insert
-               } // end outer func
-           ); // end nested handler
+                    ); // end insert
+                  } // end outer func
+           	 ); // end nested handler
 
           } // inner func
         ); // findAndModify
